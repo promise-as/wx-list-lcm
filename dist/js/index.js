45bf1668315182a3a6139294ae92764af84bc52b
Hong Baojin <13809735212@163.com>
2019-09-17 18:59:55 +0800

    basically completed

---
+++ dist/js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(function () {
+  /*
+  * operateEle: 操作元素，
+  * aimEle: 目标元素
+  * className: 类名
+  */
+  function tabHanle(operateEle, aimEle, className) {
+    $(operateEle).each(function (index) {
+      $(this).mouseenter(function () {
+        $(this).addClass(className).siblings().removeClass(className);
+        $($(aimEle)[index]).addClass(className).siblings().removeClass(className);
+      });
+    });
+  }
+
+  tabHanle('.tab-head li', '.tab-cont .cont-item', 'active');
+});
